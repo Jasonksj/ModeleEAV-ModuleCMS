78898d9f40f6ca960afe23124fa87f6a979ee8ec
JasonKsj <jasonkamsu@gmail.com>
2023-07-20 19:14:39 +0100

    moduleCMS

--- modeleEAV/src/main/java/com/example/modeleEAV/models/moduleCMS/SEOData.java
+++
@@ -1,7 +0,0 @@
-package com.example.modeleEAV.models.moduleCMS;
-
-import com.example.modeleEAV.models.utilitiesEAV.AttributeSet;
-public abstract class SEOData {
-    AttributeSet[] seoattributes;
-
-}
